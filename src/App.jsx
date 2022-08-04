f6b7300a602d51944d172698d1fa346ebeb4a663
sikander-afzaal <72333576+sikander-afzaal@users.noreply.github.com>
2022-08-04 20:14:58 +0500

    final

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,11 @@ import Particles from "react-tsparticles";
 import { loadFull } from "tsparticles";
 import gsap from "gsap";
 import "./App.css";
-import img from "./img.png";
-import logo from "./logo.png";
+import img from "./Assets/img.png";
+import logo from "./Assets/logo.png";
+import brand1 from "./Assets/brands (1).png";
+import brand2 from "./Assets/brands (2).png";
+import brand3 from "./Assets/brands (3).png";
 function App() {
   //first section--------------------
   const line1 = useRef(null);
@@ -85,33 +88,6 @@ function App() {
       stagger: 0.3,
     });
 
-    // const cardTime = gsap.timeline({
-    //   scrollTrigger: {
-    //     trigger: secondSec.current,
-    //     start: "center 15%",
-    //     end: "+=1000",
-    //     scrub: 4,
-    //   },
-    // });
-    // cardTime
-    //   .to(document.querySelector(".third-wrapper"), { yPercent: -10 })
-    //   .to(
-    //     [
-    //       card1.current,
-    //       card2.current,
-    //       card3.current,
-    //       card4.current,
-    //       card5.current,
-    //       card6.current,
-    //     ],
-    //     {
-    //       y: 0,
-    //       opacity: 1,
-    //       stagger: 0.5,
-    //     },
-    //     "0"
-    //   )
-    //   .to(scrollText.current, { y: 0, opacity: 1, delay: 0.3 }, "0");
     //third section -------------------------
 
     const pinned = gsap.timeline({
@@ -140,6 +116,7 @@ function App() {
         { x: TEXT_MOVE, duration: TEXT_SPEED, delay: 0.1 },
         "lol"
       );
+    //animation where cards slowly come up as you approach the third section
     ScrollTrigger.matchMedia({
       "(max-width: 800px)": function () {
         const cardTime = gsap.timeline({
@@ -212,7 +189,7 @@ function App() {
 
     //join section ------------------------------------------
     const h1 = document.querySelector(".join-h1");
-    const btn = document.querySelector(".btn-div");
+    const row = document.querySelector(".row");
     const mainApp = document.querySelector(".App");
     gsap.to(mainApp, {
       scrollTrigger: {
@@ -229,13 +206,10 @@ function App() {
         toggleActions: "play reverse play reverse",
       },
     });
-    lastTime.to([h1, btn], { opacity: 1, stagger: 0.5 });
+    lastTime.to([h1, row], { opacity: 1, stagger: 0.5 });
   }, []);
 
   const particlesInit = async (main) => {
-    // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
-    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-    // starting from v2 you can add only the features you need reducing the bundle size
     await loadFull(main);
   };
 
@@ -804,474 +778,18 @@ function App() {
         </div>
       </div>
       <div ref={joinSec} className="join-us">
-        {/* <Particles
-          id="tsparticles2"
-          init={particlesInit}
-          loaded={particlesLoaded}
-          options={{
-            autoPlay: true,
-            background: {
-              color: {
-                value: "white",
-              },
-              image: "",
-              position: "50% 50%",
-              repeat: "no-repeat",
-              size: "cover",
-              opacity: 1,
-            },
-            backgroundMask: {
-              composite: "destination-out",
-              cover: {
-                color: {
-                  value: "#000000",
-                },
-                opacity: 1,
-              },
-              enable: false,
-            },
-            fullScreen: {
-              enable: false,
-              zIndex: -1,
-            },
-            detectRetina: true,
-            duration: 0,
-            fpsLimit: 120,
-            interactivity: {
-              detectsOn: "window",
-              events: {
-                onClick: {
-                  enable: true,
-                  mode: "push",
-                },
-                onDiv: {
-                  selectors: [],
-                  enable: false,
-                  mode: [],
-                  type: "circle",
-                },
-                onHover: {
-                  enable: true,
-                  mode: "grab",
-                  parallax: {
-                    enable: true,
-                    force: 60,
-                    smooth: 10,
-                  },
-                },
-                resize: true,
-              },
-              modes: {
-                attract: {
-                  distance: 200,
-                  duration: 0.4,
-                  easing: "ease-out-quad",
-                  factor: 1,
-                  maxSpeed: 50,
-                  speed: 1,
-                },
-                bounce: {
-                  distance: 200,
-                },
-                bubble: {
-                  distance: 400,
-                  duration: 2,
-                  mix: false,
-                  opacity: 0.8,
-                  size: 40,
-                  divs: {
-                    distance: 200,
-                    duration: 0.4,
-                    mix: false,
-                    selectors: [],
-                  },
-                },
-                connect: {
-                  distance: 80,
-                  links: {
-                    opacity: 0.5,
-                  },
-                  radius: 60,
-                },
-                grab: {
-                  distance: 400,
-                  links: {
-                    blink: false,
-                    consent: false,
-                    opacity: 1,
-                  },
-                },
-                light: {
-                  area: {
-                    gradient: {
-                      start: {
-                        value: "#000000",
-                      },
-                      stop: {
-                        value: "#000000",
-                      },
-                    },
-                    radius: 1000,
-                  },
-                  shadow: {
-                    color: {
-                      value: "#000000",
-                    },
-                    length: 2000,
-                  },
-                },
-                push: {
-                  default: true,
-                  groups: [],
-                  quantity: 4,
-                },
-                remove: {
-                  quantity: 2,
-                },
-                repulse: {
-                  distance: 200,
-                  duration: 0.4,
-                  factor: 100,
-                  speed: 1,
-                  maxSpeed: 50,
-                  easing: "ease-out-quad",
-                  divs: {
-                    distance: 200,
-                    duration: 0.4,
-                    factor: 100,
-                    speed: 1,
-                    maxSpeed: 50,
-                    easing: "ease-out-quad",
-                    selectors: [],
-                  },
-                },
-                slow: {
-                  factor: 3,
-                  radius: 200,
-                },
-                trail: {
-                  delay: 1,
-                  pauseOnStop: false,
-                  quantity: 1,
-                },
-              },
-            },
-            manualParticles: [],
-            motion: {
-              disable: false,
-              reduce: {
-                factor: 4,
-                value: true,
-              },
-            },
-            particles: {
-              bounce: {
-                horizontal: {
-                  random: {
-                    enable: false,
-                    minimumValue: 0.1,
-                  },
-                  value: 1,
-                },
-                vertical: {
-                  random: {
-                    enable: false,
-                    minimumValue: 0.1,
-                  },
-                  value: 1,
-                },
-              },
-              collisions: {
-                bounce: {
-                  horizontal: {
-                    random: {
-                      enable: false,
-                      minimumValue: 0.1,
-                    },
-                    value: 1,
-                  },
-                  vertical: {
-                    random: {
-                      enable: false,
-                      minimumValue: 0.1,
-                    },
-                    value: 1,
-                  },
-                },
-                enable: false,
-                mode: "bounce",
-                overlap: {
-                  enable: true,
-                  retries: 0,
-                },
-              },
-              color: {
-                value: "black",
-                animation: {
-                  h: {
-                    count: 0,
-                    enable: false,
-                    offset: 0,
-                    speed: 1,
-                    decay: 0,
-                    sync: true,
-                  },
-                  s: {
-                    count: 0,
-                    enable: false,
-                    offset: 0,
-                    speed: 1,
-                    decay: 0,
-                    sync: true,
-                  },
-                  l: {
-                    count: 0,
-                    enable: false,
-                    offset: 0,
-                    speed: 1,
-                    decay: 0,
-                    sync: true,
-                  },
-                },
-              },
-              destroy: {
-                mode: "none",
-                split: {
-                  count: 1,
-                  factor: {
-                    random: {
-                      enable: false,
-                      minimumValue: 0,
-                    },
-                    value: 3,
-                  },
-                  rate: {
-                    random: {
-                      enable: false,
-                      minimumValue: 0,
-                    },
-                    value: {
-                      min: 4,
-                      max: 9,
-                    },
-                  },
-                  sizeOffset: true,
-                },
-              },
-              gradient: [],
-              groups: {},
-              links: {
-                blink: false,
-                color: {
-                  value: "#000000",
-                },
-                consent: false,
-                distance: 150,
-                enable: true,
-                frequency: 1,
-                opacity: 0.4,
-                shadow: {
-                  blur: 5,
-                  color: {
-                    value: "#000",
-                  },
-                  enable: false,
-                },
-                triangles: {
-                  enable: false,
-                  frequency: 1,
-                },
-                width: 1,
-                warp: false,
-              },
-              move: {
-                angle: {
-                  offset: 0,
-                  value: 90,
-                },
-                attract: {
-                  distance: 200,
-                  enable: false,
-                  rotate: {
-                    x: 600,
-                    y: 1200,
-                  },
-                },
-                center: {
-                  x: 50,
-                  y: 50,
-                  radius: 0,
-                },
-                decay: 0,
-                distance: {},
-                direction: "none",
-                drift: 0,
-                enable: true,
-                gravity: {
-                  acceleration: 9.81,
-                  enable: false,
-                  inverse: false,
-                  maxSpeed: 50,
-                },
-                path: {
-                  clamp: true,
-                  delay: {
-                    random: {
-                      enable: false,
-                      minimumValue: 0,
-                    },
-                    value: 0,
-                  },
-                  enable: false,
-                  options: {},
-                },
-                outModes: {
-                  default: "out",
-                  bottom: "out",
-                  left: "out",
-                  right: "out",
-                  top: "out",
-                },
-                random: false,
-                size: false,
-                speed: 2,
-                spin: {
-                  acceleration: 0,
-                  enable: false,
-                },
-                straight: false,
-                trail: {
-                  enable: false,
-                  length: 10,
-                  fillColor: {
-                    value: "#000000",
-                  },
-                },
-                vibrate: false,
-                warp: false,
-              },
-              number: {
-                density: {
-                  enable: true,
-                  area: 800,
-                  factor: 1000,
-                },
-                limit: 0,
-                value: 100,
-              },
-              opacity: {
-                random: {
-                  enable: true,
-                  minimumValue: 0.1,
-                },
-                value: {
-                  min: 0.1,
-                  max: 0.5,
-                },
-                animation: {
-                  count: 0,
-                  enable: true,
-                  speed: 3,
-                  decay: 0,
-                  sync: false,
-                  destroy: "none",
-                  startValue: "random",
-                  minimumValue: 0.1,
-                },
-              },
-              reduceDuplicates: false,
-              repulse: {
-                random: {
-                  enable: false,
-                  minimumValue: 0,
-                },
-                value: 0,
-                enabled: false,
-                distance: 1,
-                duration: 1,
-                factor: 1,
-                speed: 1,
-              },
-              rotate: {
-                random: {
-                  enable: false,
-                  minimumValue: 0,
-                },
-                value: 0,
-                animation: {
-                  enable: false,
-                  speed: 0,
-                  decay: 0,
-                  sync: false,
-                },
-                direction: "clockwise",
-                path: false,
-              },
-              shadow: {
-                blur: 0,
-                color: {
-                  value: "#000",
-                },
-                enable: false,
-                offset: {
-                  x: 0,
-                  y: 0,
-                },
-              },
-              shape: {
-                options: {},
-                type: "circle",
-              },
-              size: {
-                random: {
-                  enable: true,
-                  minimumValue: 1,
-                },
-                value: {
-                  min: 0.1,
-                  max: 10,
-                },
-                animation: {
-                  count: 0,
-                  enable: true,
-                  speed: 20,
-                  decay: 0,
-                  sync: false,
-                  destroy: "none",
-                  startValue: "random",
-                  minimumValue: 0.1,
-                },
-              },
-              stroke: {
-                width: 0,
-              },
-              zIndex: {
-                random: {
-                  enable: false,
-                  minimumValue: 0,
-                },
-                value: 0,
-                opacityRate: 1,
-                sizeRate: 1,
-                velocityRate: 1,
-              },
-            },
-            pauseOnBlur: true,
-            pauseOnOutsideViewport: true,
-            responsive: [],
-            style: {},
-            themes: [],
-            zLayers: 100,
-          }}
-        /> */}
-        <h1 className="join-h1">Join Us</h1>
-        <div className="btn-div">
-          <button className="cta-btn">Work at yuga</button>
-          <button className="cta-btn">Partner with yuga</button>
+        <h1 className="join-h1">Be part of the future</h1>
+        <p>As featured in</p>
+        <div className="row">
+          <img src={brand1} alt="" />
+          <img src={brand2} alt="" />
+          <img src={brand3} alt="" />
+          {/* <button className="cta-btn">Work at yuga</button>
+          <button className="cta-btn">Partner with yuga</button> */}
         </div>
       </div>
       <footer>
-        <h2>Yuga Labs</h2>
+        <h2>YugaLabs</h2>
         <div className="right-links">
           <a href="#">Press</a>
           <a href="#">Times</a>
