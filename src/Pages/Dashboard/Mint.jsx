707c451e59b696fe24cb1b96b146bc4885564d0b
sikander-afzaal <sikander.aaa@gmail.com>
2022-11-11 19:33:22 +0500

    done6

--- src/Pages/Dashboard/Mint.jsx
+++ src/Pages/Dashboard/Mint.jsx
@@ -5,6 +5,7 @@ import AnimatedPage from "../../Components/AnimatedPage";
 import thread from "../../Assets/thread.png";
 import text from "../../Assets/professor-text.png";
 import board from "../../Assets/board.png";
+import arrow from "../../Assets/arrow.png";
 import gif from "../../Assets/gif.gif";
 
 const Mint = () => {
@@ -14,6 +15,21 @@ const Mint = () => {
         <div className="gif-div">
           <img src={thread} className="wire" alt="" />
           <img src={gif} className="gif" alt="" />
+        </div>
+        {/* <div className="code-text">
+          <p>!!! NO CODE !!! CLICK THE ALPHA BUTTON TO GET ONE FREE !!!!!</p>
+          <img src={arrow} alt="" />
+        </div> */}
+        <div className="list-text">
+          <h3 className="cp">FREE MINT FEATURES</h3>
+          <ul>
+            <li className="cp"> 10,000 Unique Professors</li>
+            <li className="cp">
+              Free Access To 'LABZ' Tech Builds In Professor AMA Lounge
+            </li>
+            <li className="cp">Whitelist Spot For Wagmilabz Collections</li>
+            <li className="cp">Wagmi Market place access</li>
+          </ul>
         </div>
         <img src={board} alt="" className="mint-board" />
         {/* <img src={prof} alt="" className="prof" /> */}
